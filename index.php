f4f7b5712f0356975d952a799930133e6d810b80
Enoch Tsang <etsang1@hotmail.com>
2015-11-04 16:48:30 -0700

    added missing div tag

--- index.php
+++ index.php
@@ -1,26 +1,27 @@
 <!DOCTYPE html>
 <html lang="en-CA">
 <head>
-	<?php include 'header.php';?>
-	<title>Flock Information Database</title>
-	<meta name="description" content="A tool for the Institute of Applied Poultry Technology (IAPT) to enter and store flock information.">
+    <?php include 'header.php';?>
+    <title>Flock Information Database</title>
+    <meta name="description" content="A tool for the Institute of Applied Poultry Technology (IAPT) to enter and store flock information.">
 </head>
 <body>
-	<div class="wrapper">
-		<div class="title">
-			<h1>Flock Information Database</h1>
-		</div>
-	<?php include 'navigation-bar.php'; ?>
-	</div>
-	<div class="content">
-		<div class="summary">
-			<h1> Home </h1>
-			<br>
-			<p id="About_Us">
-				Here is the home page!
-			</p>
-			<img src="resources/chicken-cat.jpg" 
-			 alt="Chicken Cat" />
-	</div>
+    <div class="wrapper">
+        <div class="title">
+            <h1>Flock Information Database</h1>
+        </div>
+    <?php include 'navigation-bar.php'; ?>
+    </div>
+    <div class="content">
+        <div class="summary">
+            <h1> Home </h1>
+            <br>
+            <p id="About_Us">
+                Here is the home page!
+            </p>
+            <img src="resources/chicken-cat.jpg" 
+             alt="Chicken Cat" />
+        </div>
+    </div>
 </body>
 </html>
