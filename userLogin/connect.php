7b41551a46ee37f533ad64afd5ab41792ed92181
jeinglis <jeinglis@ucalgary.ca>
2015-11-05 15:11:59 -0700

    altered the connect.php

--- userLogin/connect.php
+++ userLogin/connect.php
@@ -1,4 +1,4 @@
 <?php
-$con = mysqli_connect ( "localhost", "jeinglis", "cavall01", "mydb" ) or die ( "Some error occurred during connection " . mysqli_error ( $con ) );
+$con = mysqli_connect ( "localhost", "jeinglis", "password", "mydb" ) or die ( "Some error occurred during connection " . mysqli_error ( $con ) );
 ?>
    
