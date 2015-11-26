3644852849662cd488da812b84262c066d7db30e
Enoch Tsang <etsang1@hotmail.com>
2015-11-26 15:00:51 -0700

    general pages with colors

---
+++ government-lookup.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en-CA">
+<head>
+    <?php include 'header.php';?>
+    <title>Flock Information Database - IAPT Lookup</title>
+    <meta name="description" content="View the flock information statistics owned by the Institute of Applied Poultry Technology (IAPT).">
+</head>
+<body>
+    <div class="wrapper">
+        <div id="logo-bar">
+            <img src="resources/chicken-cat.jpg" alt="Chicken Cat" height="42", width="42" />
+            <h1> IAPT Information </h1>
+        </div>        
+        <div class="content">
+            <h2> Producer/Enterprise Name: </h2>
+        </div>
+    </div>
+</body>
+</html>
--- submit-form.php
+++ submit-form.php
@@ -2,30 +2,17 @@
 <html lang="en-CA">
 <head>
     <?php include 'header.php';?>
-    <title>Flock Information Database - Statistics</title>
-    <meta name="description" content="View the flock information statistics owned by the Institute of Applied Poultry Technology (IAPT).">
+    <title>Flock Information Database - Submit Form</title>
+    <meta name="description" content="Submit a flock information form to the database for review by IAPT">
 </head>
 <body>
     <div class="wrapper">
-        <div class="title">
-            <h1>Flock Information Database</h1>
-        </div>
-    <?php include 'navigation-bar.php'; ?>
-    </div>
-    <div class="content">
-        <div class="summary">
-            <h1> Statistics </h1>
-            <br>
-            <p id="About_Us">
-                Here is lots of statistics!
-            </p>
-        </div>
-        <div class="form_results">
-            <p>
-                Barn Number is: <?php echo $_POST["barn_no"]; ?>
-                <br><br>
-                Vaccine Administered is: <?php echo $_POST["vaccine_administered"]; ?>
-            </p>
+        <div id="logo-bar">
+            <img src="resources/chicken-cat.jpg" alt="Chicken Cat" height="42", width="42" />
+            <h1> Flock Information Reporting Form </h1>
+        </div>        
+        <div class="content">
+            <h2> Producer/Enterprise Name: </h2>
         </div>
     </div>
 </body>
