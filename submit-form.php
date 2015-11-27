1ab912b45bf13dab3f734a721da4d6661675ce12
Enoch Tsang <etsang1@hotmail.com>
2015-11-27 13:46:46 -0700

    changed some colors and spaced out form fields

--- submit-form.php
+++ submit-form.php
@@ -6,47 +6,43 @@
     <meta name="description" content="Submit a flock information form to the database for review by IAPT">
 </head>
 <body>
-    <div class="wrapper">
-        <div id="logo-bar">
-            <?php include 'logo-include.php'; ?>
-            <h1> Flock Information Reporting Form </h1>
-        </div>        
-        <div class="center-content">
-            <div class="content">
-                <div class="form-container">
-                <form name="general-flock-information" method="post" action="CHANGE_THIS.php">
-                    <div class="general">
-                        <ul>                    
-                            <li>
-                                <label for="producer-enterprise">Producer / Enterprise</label>
-                                <input type="text" name="producer-enterprise" maxlength="80">
-                            </li>
-                            <li>
-                                <label for="producer-code-quota">Producer Code / Quota</label>
-                                <input type="text" name="producer-code-quota" maxlength="80">
-                            </li>
-                            <li>
-                                <label for="barn-number">Barn #</label>
-                                <input type="text" name="barn-number" maxlength="80">
-                            </li>
-                            <li>
-                                <label for="species">Species</label>
-                                <input type="text" name="species" maxlength="80">
-                            </li>
-                            <li>
-                                <label for="category-sex">Category/Sex</label>
-                                <input type="text" name="category-sex" maxlength="80">
-                            </li>
-                            <li>
-                                <label for="age-of-birds">Age of Birds</label>
-                                <input type="text" name="age-of-birds" maxlength="80">
-                            </li>
-                        </ul>
-                    </div>
-                </form>
-                </div>
+    <div id="logo-bar">
+        <?php include 'logo-include.php'; ?>
+        <h1> Flock Information Reporting Form </h1>
+    </div>        
+    <div class="content">
+            <div class="form-container">
+            <form name="general-flock-information" method="post" action="CHANGE_THIS.php">
+                <div class="general">
+                    <table>
+                        <tr>
+                            <td width="30%"><label for="producer-enterprise">Producer / Enterprise</label></td>
+                            <td width="70%"><input type="text" name="producer-enterprise" maxlength="80"></td>
+                        </tr>
+                        <tr>
+                            <td><label for="producer-code-quota">Producer Code / Quota</label></td>
+                            <td><input type="text" name="producer-code-quota" maxlength="80"></td>
+                        </tr>
+                        <tr>
+                            <td><label for="barn-number">Barn #</label></td>
+                            <td><input type="text" name="barn-number" maxdength="80"></tr>
+                        </tr>
+                        <tr>
+                            <td><label for="species">Species</label></td>
+                            <td><input type="text" name="species" maxldngth="80"></tr>
+                        </tr>
+                        <tr>
+                            <td><label for="category-sex">Category/Sex</label></td>
+                            <td><input type="text" name="category-sex" maxlengthd"80"></tr>
+                        </tr>
+                        <tr>
+                            <td><label for="age-of-birds">Age of Birds</label></td>
+                            <td><input type="text" name="age-of-birds" maxlengthd"80"></tr>
+                        </tr>
+                    </table>
+                </div>  
+            </form>
             </div>
-        </div>
     </div>
 </body>
 </html>
