1ae52d78f2771088b7eaf08e99a8e1690e376efd
Jackie Luc <jaluc@ucalgary.ca>
2015-11-30 20:15:19 -0700

    Styled home page by inserting two tables for register and login into one larger table that is above the 'about us' section. May need to add padding between the register/login and 'about us'.

--- index.php
+++ index.php
@@ -13,24 +13,56 @@
         </div>
 
         <div class="content">
+            <table id="register-login">
+            <tr>
+            <td><form method="post" action="index.php">
+                <table id="inner-register">
+                    <th colspan="2"><h3>New users register here.</h3></th>
+                    <tr>
+                        <td><b>Username</b></td>
+                        <td><input type="text" name="username">
+                    </tr>
+                    <tr>
+                        <td><b>Password</b></td>
+                        <td><input name="password" type="password"></input></td>
+                    </tr> <br/>
+                    <tr>
+                        <td><b>User Type</b></td>
+                        <td><input type="text" name="user_type">
+                    </tr>
+                    <tr>
+                        <td><b>Email</b></td>
+                        <td><input type="text" name="email">
+                    </tr>
+                    <tr>
+                        <td><td><input type="submit" value="Register"/>
+                    </tr>
+                </table>
+            </form></td>
+
+            <td><form method="post" action="redirectPostLogin.php" >
+                <table id="inner-login">
+                    <th colspan="2"><h3>Existing users login here.</h3></th>
+                    <tr>
+                        <td><b>Username</b></td>
+                        <td><input type="text" name="username">
+                    </tr>
+                    <tr>
+                        <td><b>Password</b></td>
+                        <td><input name="password" type="password"></input></td>
+                    </tr> <br/>
+                    <tr>
+                        <td><td><input type="submit" value="Login"/>
+                    </tr>
+                </table>
+            </form></td>
+            </tr>
+            </table>
+
             <div id="about-us">
-            <h1> About Us </h1>
-            <p>
-                Institute of Applied Poultry Technologies! INFO HERE
-            </p>
+                <h1> About Us </h1>
+                    <p>Institute of Applied Poultry Technologies! INFO HERE</p>
             </div>
-
-            <div id="reg-login">
-                <div>
-                    <h3>New users register here.</h3>
-                    <a href="register.php"><button> Register </button></a>
-                </div>
-                <div>
-                    <h3>Existing users log in here.</h3>
-                    <a href="login.php"><button> Login </button></a>
-                </div>
-            </div>
-
         </div>
     </div>
 </body>
