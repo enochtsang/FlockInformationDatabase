8a27c8d4ed58eebebbbbdb9e1ac65a1aae7a55af
mfalthaw <Muaz@Muazs-MacBook-Air.local>
2015-11-30 20:31:22 -0700

    saved fields into variables, from planned-catching-month (line 491-end)

--- submit-form.php
+++ submit-form.php
@@ -488,6 +488,7 @@ require_once 'connect.php';
                                             <option value="nov">Nov</option>
                                             <option value="dec">Dec</option>
                                         </select>
+                                        <?php $planned_catching_month = filter_input(INPUT_POST, 'planned-catching-month'); ?>
                                     </td>
                                     <td>
                                         <label for="planned-catching-day">Day</label>
@@ -495,12 +496,14 @@ require_once 'connect.php';
                                     <td>
                                         <input type="number" name="planned-catching-day" min="1" max="31">
                                     </td>
+                                    <?php $planned_catching_day = filter_input(INPUT_POST, 'planned-catching-day'); ?>
                                 </table>
                             </td>
                             <td>
                                 <table>
                                     <td><label for="planned-catching-time">Time</label></td>
                                     <td><input type="time" name="planned-catching-time"></td>
+                                    <?php $planned_catching_time = filter_input(INPUT_POST, 'planned-catching-time'); ?>
                                 </table>
                             </td>
                         </tr>
@@ -511,6 +514,7 @@ require_once 'connect.php';
                                 <table>
                                     <td><label for="planned-catching-time">Time</label></td>
                                     <td><input type="time" name="planned-catching-time"></td>
+                                    <?php $planned_catching_time = filter_input(INPUT_POST, 'planned-catching-time'); ?>
                                 </table>
                             </td>
                         </tr>
@@ -536,12 +540,14 @@ require_once 'connect.php';
                                             <option value="nov">Nov</option>
                                             <option value="dec">Dec</option>
                                         </select>
+                                        <?php $planned_processing_month = filter_input(INPUT_POST, 'planned-processing-month'); ?>
                                     </td>
                                     <td>
                                         <label for="planned-processing-day">Day</label>
                                     </td>
                                     <td>
                                         <input type="number" name="planned-processing-day" min="1" max="31">
+                                        <?php $planned_processing_day = filter_input(INPUT_POST, 'planned-processing-day'); ?>
                                     </td>
                                 </table>
                             </td>
@@ -549,6 +555,7 @@ require_once 'connect.php';
                                 <table>
                                     <td><label for="planned-processing-time">Time</label></td>
                                     <td><input type="time" name="planned-processing-time"></td>
+                                    <?php $planned_processing_time = filter_input(INPUT_POST, 'planned-processing-time'); ?>
                                 </table>
                             </td>
                         </tr>
@@ -559,6 +566,7 @@ require_once 'connect.php';
                                 <table>
                                     <td><label for="last-water-access-time">Time</label></td>
                                     <td><input type="time" name="last-water-access-time"></td>
+                                    <?php $last_water_access_time = filter_input(INPUT_POST, 'last-water-access-time'); ?>
                                 </table>
                             </td>
                         </tr>
@@ -574,6 +582,7 @@ require_once 'connect.php';
                                         <input type="radio" name="feed-distruption" value="no">
                                         <label for="no">No</label>
                                     </td>
+                                    <?php $feed_distruption = filter_input(INPUT_POST, 'feed-distruption'); ?>
                                 </table>
                             </td>
                             <td></td>
@@ -585,6 +594,7 @@ require_once 'connect.php';
                                 <table>
                                     <td><label for="feed-withdrawal-time">Time</label></td>
                                     <td><input type="time" name="feed-withdrawal-time"></td>
+                                    <?php $feed_withdrawal_time = filter_input(INPUT_POST, 'feed-withdrawal-time'); ?>
                                 </table>
                             </td>
                         </tr>
@@ -610,6 +620,7 @@ require_once 'connect.php';
                                             <option value="nov">Nov</option>
                                             <option value="dec">Dec</option>
                                         </select>
+                                        <?php $food_no_access_month = filter_input(INPUT_POST, 'food-no-access-month'); ?>
                                     </td>
                                     <td>
                                         <label for="food-no-access-day">Day</label>
@@ -617,6 +628,7 @@ require_once 'connect.php';
                                     <td>
                                         <input type="number" name="food-no-access-day" min="1" max="31">
                                     </td>
+                                    <?php $food_no_access_day = filter_input(INPUT_POST, 'food-no-access-day'); ?>
                                 </table>
                             </td>
                             <td>
@@ -629,6 +641,7 @@ require_once 'connect.php';
                                 <input type="time" class="floor-input" name="floor-1-time">
                             </td>
                             <td></td>
+                            <?php $floor_1_time = filter_input(INPUT_POST, 'floor-1-time'); ?>
                         </tr>
                         <tr>
                             <td></td>
@@ -637,6 +650,7 @@ require_once 'connect.php';
                                 <input type="time" class="floor-input" name="floor-2-time">
                             </td>
                             <td></td>
+                            <?php $floor_2_time = filter_input(INPUT_POST, 'floor-2-time'); ?>
                         </tr>
                         <tr>
                             <td></td>
@@ -645,6 +659,7 @@ require_once 'connect.php';
                                 <input type="time" class="floor-input" name="floor-3-time">
                             </td>
                             <td></td>
+                            <?php $floor_3_time = filter_input(INPUT_POST, 'floor-3-time'); ?>
                         </tr>
                     </table>
                 </div>
