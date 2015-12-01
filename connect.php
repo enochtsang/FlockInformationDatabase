59db8727d792df30fe8fe09fe1af2b4b8b0a5139
Jackie Luc <jaluc@ucalgary.ca>
2015-12-01 11:57:20 -0700

    removed unneccessary stuff and fixed some errors for login

--- connect.php
+++
@@ -1,4 +0,0 @@
-<?php
-$con = mysqli_connect("localhost","root","","flock-info-db") or die("Some error occurred during connection " . mysqli_error($con));  
-?>
-   
--- navigation-bar.php
+++
@@ -1,8 +0,0 @@
-<div class="navigation-bar">
-    <ul id="navigation-items">
-        <li><a href="index.php"> Home </a></li>
-        <li><a href="statistics.php"> Statistics </a></li>
-        <li><a href="submit-form.php"> Submit a form </a></li>
-        <li><a href="contact.php"> Contact </a></li>
-    </ul>
-</div>
