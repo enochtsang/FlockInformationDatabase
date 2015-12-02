ef2a5f22fbe774aff21da43321722b24e124a7a0
Enoch Tsang <etsang1@hotmail.com>
2015-12-01 18:26:52 -0700

    readded header.php

---
+++ header.php
@@ -0,0 +1,7 @@
+<meta charset="UTF-8">
+<link href="resources/stylesheet.css?version=0" rel="stylesheet" type="text/css"/>
+<meta name="author" content="Enoch Tsang, James Inglis, Moath Faisal, Jackie Luc">
+<link rel="icon" type="image/png" href="resources/favicon.ico" />
+<link rel="stylesheet" href="/resources/libraries/jquery-ui/jquery-ui.min.css">
+<script src="resources/libraries/jquery-1.11.3.js"></script>
+<script src="resources/libraries/jquery-ui/jquery-ui.min.js"></script>
