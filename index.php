cee1259e9907976f2510bcf2f2b6abd7c5874a82
Jackie Luc <jaluc@ucalgary.ca>
2015-12-01 17:50:01 -0700

    reverted 'label' names of register/login input for username and password

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
+<?php
+require_once 'connect.php';
+
+// If the values are posted, insert them into the database.
+if (isset($_POST['username']) && isset($_POST['user_type']) && isset($_POST['password']) && isset($_POST['email'])) {
+    $username = $_POST['username'];
+    $user_type = $_POST['user_type'];
+    $password = $_POST['password'];
+    $email = $_POST['email'];
+    
+    $query = "SELECT username FROM `user` WHERE username='$username'";
+    $isDuplicate = mysqli_query($con, $query);
+    if($isDuplicate) {
+        $msg = "Sorry! This username is currently taken. Please choose another one.";
+    }    
+    
+    if (!empty($username) && !empty($user_type) && !empty($password) && !empty($email)) {
+        $query = "INSERT INTO `user` (username, password, user_type, email) VALUES ('$username', '$password','$user_type', '$email')";
+        $result = mysqli_query($con, $query);
+        if ($result) {
+            $msg = "User Created Successfully.";
+        }
+    } else {
+        $msg = "Please fill in all the fields.";
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en-CA">
 <head>
@@ -20,12 +47,12 @@
                     <table id="inner-register">
                         <th colspan="2"><h3>New users register here.</h3></th>
                         <tr>
-                            <td><label for="register_username"><b>Username<b></label></td>
-                            <td><input type="text" name="register_username"></td>
+                            <td><label for="username"><b>Username<b></label></td>
+                            <td><input type="text" name="username"></td>
                         </tr>
                         <tr>
-                            <td><label for="register_password"><b>Password</label></b></td>
-                            <td><input name="register_password" type="password"></input></td>
+                            <td><label for="password"><b>Password</label></b></td>
+                            <td><input type="password" name="password"></input></td>
                         </tr>
                         <tr>
                             <td><label for="user_type"><b>User Type</b></label></td>
@@ -47,12 +74,12 @@
                     <table id="inner-login">
                         <th colspan="2"><h3>Existing users login here.</h3></th>
                         <tr>
-                            <td><label for="login_username"><b>Username</b></label></td>
-                            <td><input type="text" name="login_username"></td>
+                            <td><label for="username"><b>Username</b></label></td>
+                            <td><input type="text" name="username"></td>
                         </tr>
                         <tr>
                             <td><label for="login_password"><b>Password</b></label></td>
-                            <td><input name="password" type="login_password"></input></td>
+                            <td><input name="password" type="password"></input></td>
                         </tr>
                         <tr>
                             <td colspan="2"><input type="submit" value="Login"/></td>
@@ -63,6 +90,14 @@
             </tr>
             </table>
 
+            <div>
+            <br>
+            <?php
+            if (isset($msg) & !empty($msg)) {
+                echo $msg;
+            }
+            ?>  
+            </div>
             <div id="about-us">
                 <h1> About Us </h1>
                 <p>Institute of Applied Poultry Technologies! INFO HERE</p>
